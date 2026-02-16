3a587cbcb19ce0ddd27397ad220ecfa5bcae254c
AybukeYaman <aybukeyaman5806@gmail.com>
2026-02-17 00:50:45 +0300

    Update: First Page Column / Vertical Design Has Done

--- lib/your_ai_stylist_page.dart
+++ lib/your_ai_stylist_page.dart
@@ -10,17 +10,7 @@ class YourAiStylistPage extends StatefulWidget {
 
 class _YourAiStylistPageState extends State<YourAiStylistPage>
     with SingleTickerProviderStateMixin {
-  late AnimationController animationController;
-
-  void _initState() {
-    animationController = AnimationController(
-      vsync: this,
-      duration: Duration(seconds: 5),
-    );
-    animationController.repeat();
-    super.initState();
-  }
-
+      
   @override
   Widget build(BuildContext context) {
     final deviceOrientation = MediaQuery.of(context).orientation;
@@ -37,18 +27,15 @@ class _YourAiStylistPageState extends State<YourAiStylistPage>
             stops: [0.2, 0.6],
           ),
         ),
-
         child: Container(
-          child: myColumn(1, 1),
-          /*  child: deviceOrientation == Orientation.portrait
-              ? myColumn(screenHeight, screenWidth)
-              : myRow(screenHeight, screenWidth), */
+          child: deviceOrientation == Orientation.portrait
+              ? myColumn(1, 1)
+              : myRow(1, 1),
         ),
       ),
     );
   }
 
-  //BURA DİKE EKRANIN CONTAİNERLARININ BİRİM GENİŞLİK VE UZUNLUKLARINI VERİYOR ORANSAL OLARAK
   Column myColumn(double screenHeight, double screenWidth) {
     return Column(
       children: [
@@ -56,63 +43,13 @@ class _YourAiStylistPageState extends State<YourAiStylistPage>
 
         Column(
           children: [
-            Container(
-              height: MediaQuery.of(context).size.height * 0.075,
-              width: MediaQuery.of(context).size.width * 1,
-              child: Text(
-                "Your AI Stylist",
-                style: TextStyle(
-                  color: Color(0xFF1A1A1A),
-                  fontFamily: "PT_Serif",
-                  fontWeight: FontWeight.w600,
-                  fontSize: 40,
-                ),
-                textAlign: TextAlign.center,
-              ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.125,
-              width: MediaQuery.of(context).size.width * 0.8,
-
-              child: Center(
-                child: Text(
-                  "Get personalized outfit recommendations based on your wardrobe, mood, and the weather",
-                  style: TextStyle(
-                    color: Color.fromARGB(255, 91, 93, 93),
-                    fontFamily: "Roboto",
-                    fontWeight: FontWeight.w400,
-                    fontSize: 20,
-                  ),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            ),
+            firstTextSizedBox(0.075, 1, 40),
+            secondTextSizedBox(0.125, 0.8, 20),
           ],
         ),
 
         //buaraya animasyon gelecek
-        Container(
-          height: MediaQuery.of(context).size.height * 0.05,
-          width: MediaQuery.of(context).size.width * 0.05,
-          decoration: BoxDecoration(
-            color: Color(0xFFDB7964),
-            shape: BoxShape.circle,
-
-            boxShadow: [
-              BoxShadow(
-                color: Color.fromARGB(
-                  255,
-                  216,
-                  135,
-                  117,
-                ).withValues(alpha: 0.2),
-                spreadRadius: 20,
-                blurRadius: 20,
-              ),
-            ],
-          ),
-          alignment: AlignmentGeometry.center,
-        ),
+        sliderContainer(0.05, 0.05),
 
         Container(
           height: MediaQuery.of(context).size.height * 0.09,
@@ -152,50 +89,115 @@ class _YourAiStylistPageState extends State<YourAiStylistPage>
             ),
           ),
         ),
-
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.125,
-          width: MediaQuery.of(context).size.width * 1,
-          child: Text(
-            "I already have an account",
-            style: TextStyle(
-              color: Color.fromARGB(255, 91, 93, 93),
-              fontFamily: "Roboco",
-              fontWeight: FontWeight.w400,
-              fontSize: 17,
-            ),
-            textAlign: TextAlign.center,
-          ),
-        ),
+        accountTextSizedBox(0.125, 1),
       ],
     );
   }
 
-  /* 
-  Row myRow(double sH, double sW) {
+  Row myRow(double screenHeight, double screenWidth) {
     return Row(
       children: [
-        Center(child: ticketContainer(screenHeight, screenWidth, 5)),
-        Container(
-          height: screenHeight * 0.33,
-          width: screenWidth * 0.33,
-          decoration: BoxDecoration(
-            color: Color(0xFFDB7964),
-            shape: BoxShape.circle,
-          ),
+        Center(child: ticketContainer(0.30, 0.50)),
+
+        Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            firstTextSizedBox(0.14, 0.35, 32),
+            secondTextSizedBox(0.3, 0.35, 17),
+
+            //buaraya animasyon gelecek
+            sliderContainer(0.05, 0.35),
+          ],
         ),
-        Container(
-          height: screenHeight * 0.33,
-          width: screenWidth * 0.33,
-          decoration: BoxDecoration(
-            color: Color(0xFFDB7964),
-            shape: BoxShape.circle,
-          ),
+
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.15, //
+              width: MediaQuery.of(context).size.width * 0.25, //
+              decoration: BoxDecoration(
+                color: Color(0xFFDB7964),
+                borderRadius: BorderRadius.circular(50),
+                boxShadow: [
+                  BoxShadow(
+                    color: Color.fromARGB(
+                      255,
+                      216,
+                      135,
+                      117,
+                    ).withValues(alpha: 0.2),
+                    spreadRadius: 20,
+                    blurRadius: 20,
+                  ),
+                ],
+              ),
+              alignment: Alignment.center,
+              child: Text(
+                "Get Started",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontFamily: "Roboto",
+                  fontWeight: FontWeight.bold,
+                  fontSize: 19,
+                  shadows: <Shadow>[
+                    Shadow(
+                      offset: Offset(0.0, 2.0),
+                      blurRadius: 4.0,
+                      color: Colors.white.withValues(alpha: 0.1),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+
+            accountTextSizedBox(0.05, 0.35),
+          ],
         ),
       ],
     );
   }
- */
+
+  /////////////////////////////////////////////////////////////////////////////
+  //Insider Methods
+  SizedBox accountTextSizedBox(double sH, double sW) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * sH,
+      width: MediaQuery.of(context).size.width * sW,
+      child: Text(
+        "I already have an account",
+        style: TextStyle(
+          color: Color.fromARGB(255, 91, 93, 93),
+          fontFamily: "Roboco",
+          fontWeight: FontWeight.w400,
+          fontSize: 17,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  Container sliderContainer(double sH, double sW) {
+    return Container(
+      height: MediaQuery.of(context).size.height * sH,
+      width: MediaQuery.of(context).size.width * sW, //
+      decoration: BoxDecoration(
+        color: Color(0xFFDB7964),
+        shape: BoxShape.circle,
+        boxShadow: [
+          BoxShadow(
+            color: Color.fromARGB(255, 216, 135, 117).withValues(alpha: 0.2),
+            spreadRadius: 20,
+            blurRadius: 20,
+          ),
+        ],
+      ),
+      alignment: Alignment.center,
+    );
+  }
+
   Container ticketContainer(double sW, double sH) {
     return Container(
       height: MediaQuery.of(context).size.height * sH,
@@ -217,6 +219,43 @@ class _YourAiStylistPageState extends State<YourAiStylistPage>
             (MediaQuery.of(context).size.width /
             (MediaQuery.of(context).size.width / 120)),
         color: Colors.white,
+      ),
+    );
+  }
+
+  SizedBox firstTextSizedBox(double sH, double sW, double fS) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * sH,
+      width: MediaQuery.of(context).size.width * sW,
+      child: Text(
+        "Your AI Stylist",
+        style: TextStyle(
+          color: Color(0xFF1A1A1A),
+          fontFamily: "PT_Serif",
+          fontWeight: FontWeight.w600,
+          fontSize: fS,
+        ),
+        textAlign: TextAlign.center,
+      ),
+    );
+  }
+
+  SizedBox secondTextSizedBox(double sH, double sW, double fS) {
+    return SizedBox(
+      height: MediaQuery.of(context).size.height * sH,
+      width: MediaQuery.of(context).size.width * sW,
+
+      child: Center(
+        child: Text(
+          "Get personalized outfit recommendations based on your wardrobe, mood, and the weather",
+          style: TextStyle(
+            color: Color.fromARGB(255, 91, 93, 93),
+            fontFamily: "Roboto",
+            fontWeight: FontWeight.w400,
+            fontSize: fS,
+          ),
+          textAlign: TextAlign.center,
+        ),
       ),
     );
   }
